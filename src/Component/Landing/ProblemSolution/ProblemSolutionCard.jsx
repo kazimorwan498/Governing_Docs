057cee8f882616a7d414b5ec710090ecd5bd4934
kazimorwan498 <kazimdmorwan498@gmail.com>
2025-01-31 23:41:52 +0600

    ProblemSolution Section is Fully Complete

--- src/Component/Landing/ProblemSolution/ProblemSolutionCard.jsx
+++ src/Component/Landing/ProblemSolution/ProblemSolutionCard.jsx
@@ -1,13 +1,13 @@
 export default function ProblemSolutionCard({ img, title, des1, des2 }) {
 	return (
-		<div className="w-[272px] h-[349px] rounded-[20px] p-5 space-y-5 shadow-ps-card bg-white hover:bg-primary group/ps transition-colors border border-white">
+		<div className="w-[272px] h-[349px] rounded-[20px] p-5 space-y-5 shadow-ps-card bg-white hover:bg-primary active:bg-primary group/ps transition-colors border border-white">
 			<div className="flex items-center justify-center bg-linear-(--gradient-primary) size-[58px] rounded-full">
 				<img src={img} alt={title} />
 			</div>
 			<h4 className="font-bold text-lg text-secondary group-hover/ps:text-white leading-none">
 				{title}
 			</h4>
-			<div className="text-accent space-y-2.5 group-hover/ps:text-white leading-">
+			<div className="text-accent space-y-2.5 group-hover/ps:text-white leading-[1.45]">
 				<p>{des1}</p>
 				{des2 && <p>{des2}</p>}
 			</div>
