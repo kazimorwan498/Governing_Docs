399daeb35cda7ec4b22b1a0c493abbaff28f305b
kazimorwan498 <kazimdmorwan498@gmail.com>
2025-01-31 18:38:06 +0600

    Hero Section is Done

--- src/Component/Landing/Hero.jsx
+++ src/Component/Landing/Hero.jsx
@@ -3,13 +3,13 @@ import Container from "../Global/Container";
 
 export default function Hero() {
 	return (
-		<section className="pt-[142px] pb-[128px]">
-			<Container className="flex justify-center items-center gap-0">
-				<div className="space-y-[29px]">
-					<h1 className="font-SpaceGrotesk font-bold text-[60px] leading-[76px] max-w-[593px]">
+		<section className="pt-10 md:pt-20 lg:pt-28 xl:pt-[142px] pb-[128px] overflow-x-hidden">
+			<Container className="flex flex-col-reverse md:flex-row justify-center items-center gap-x-0 gap-y-5">
+				<div className="space-y-5 md:space-y-[29px]">
+					<h1 className="font-SpaceGrotesk font-bold text-3xl md:text-4xl lg:text-6xl leading-[35px] md:leading-[50px] lg:leading-[76px] max-w-[593px]">
 						Simplify your real estate document analysis
 					</h1>
-					<p className="font-semibold text-xl max-w-[519px]">
+					<p className="font-semibold text-base md:text-lg lg:text-xl max-w-[350px] sm:max-w-[400px] md:max-w-[519px]">
 						Streamline, Automate, and Secure Your Business Documents
 						with Ai GoverningDocs
 					</p>
@@ -19,7 +19,7 @@ export default function Hero() {
 					<img
 						src="/hero-img.svg"
 						alt="hero-img.svg"
-						className="min-w-[740px] min-h-[640px] mx-auto mr-[-67px] flex"
+						className="size-full xl:min-w-[740px] xl:min-h-[640px] max-h-[400px] max-w-auto md:max-w-[550px] md:max-h-[640px] mx-auto xl:mr-[-67px]"
 					/>
 				</div>
 			</Container>
--- src/Component/Shared/MobileNavbar.jsx
+++ src/Component/Shared/MobileNavbar.jsx
@@ -1,11 +1,12 @@
 import { MobileNavItems } from "../Global/NavItems";
 import Logo from "../Global/Logo";
 
+// eslint-disable-next-line react/prop-types
 export default function MobileNav({ toggleMenu }) {
 	return (
 		<div className="min-[940px]:hidden absolute top-0 left-0 w-full h-screen bg-white z-50 p-6 overflow-y-hidden">
 			<div className="flex justify-between items-center pb-6">
-				<Logo />
+				<Logo>GoverningDocs</Logo>
 				<button
 					className="text-black text-2xl p-2 cursor-pointer rounded-full hover:bg-gray-100 active:bg-gray-200 transition-colors"
 					aria-label="Close Menu"
